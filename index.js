6264b001b78caaea1678348fb5676cec91f19703
GiovanniCortes <giocortes19@gmail.com>
2023-03-11 21:22:47 -0400

    made delete-btn work with double click

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 //functionality
 
@@ -16,6 +17,12 @@ inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+  myLeads = [];
+  localStorage.clear();
   renderLeads();
 });
 
